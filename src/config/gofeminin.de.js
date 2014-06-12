3bb8ea0b8aae21a262bc9c94f624d2fee3ed67d7
Marco Kellershoff <me@walialu.com>
2014-06-12 10:47:48 +0200

    Rename to camelCase and textual changes.

--- src/config/gofeminin.de.js
+++ src/config/gofeminin.de.js
@@ -2,7 +2,7 @@
   "id": 34,
   "siteID": 4,
   "mainURL": "http://localhost:4000",
-  "counter_pixel_method_name": "call_IVW",
+  "counterPixelMethodName": "call_IVW",
   "callTimerFunctionName": "af_addReload",
   "callTimerFunctionTime": 9000
 }
--- src/config/onmeda.de.js
+++ src/config/onmeda.de.js
@@ -1,8 +1,7 @@
 {
   "id": 34,
   "siteID": 4,
-  "mainURL": "http://localhost:4000",
-  "counter_pixel_method_name": "call_IVW",
-  "callTimerFunctionName": "af_addReload",
-  "callTimerFunctionTime": 9000
+  "counterPixelMethodName": "setIVWPixel",
+  "callTimerFunctionName": "",
+  "callTimerFunctionTime": ""
 }
