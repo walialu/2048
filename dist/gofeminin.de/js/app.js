5ba21cf13616bf26d8a5507234392939eb263d61
Mischa Wagner <mischa.wagner@gofeminin.de>
2014-06-24 14:50:55 +0200

    [FIXED] Leerzeichen zwischen 2048 und zu....

--- dist/gofeminin.de/js/app.js
+++ dist/gofeminin.de/js/app.js
@@ -1004,7 +1004,7 @@ window.requestAnimationFrame(function () {
 });window.GofemHelper.gofem2048Game.setLanguage({
   "general": {
     "game_title": "2048",
-    "game_subline": "Versuche die Zahl <strong>2048</strong>zu erreichen!",
+    "game_subline": "Versuche die Zahl <strong>2048</strong> zu erreichen!",
     "how_to_text_short": "<strong>Spielanleitung:</strong> Um die Kacheln zu bewegen, verwenden Sie auf Ihrem Desktop-Rechner die <strong>Pfeiltasten</strong> (oben, unten, rechts, links). Bei mobiler Nutzung bewegen Sie die Kacheln mit einer <strong>Wischbewegung</strong> in die gewünschte Richtung. Wenn sich zwei Zahlenfelder mit der gleichen Zahl berühren, entsteht ein Zahlenfeld mit deren Summe. Das Ziel ist das Zahlenfeld <strong>2048</strong> zu erreichen.",
     "label_game_page": "Zeit",
     "text_game_end_page": "Sie haben gewonnen",
